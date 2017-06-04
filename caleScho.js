9200decbb9ecc2e28c0d652df866e99df0830c53
fengxxc <744320491@qq.com>
2017-06-04 23:42:38 +0800

    编辑按钮改用纯css3编辑；代码调优

--- caleScho.js
+++ caleScho.js
@@ -4,48 +4,24 @@
 */
 ;(function($){
 	$.fn.caleScho = function() {
-		console.time();
-		
-		var $self = $(this);
-		// arguments[0] is 'options'
-		if (!arguments.length || typeof arguments[0] == 'object') {
-
-			var opts = arguments[0] || {};
-			
-			// 初始年，默认为当前年
-			var initYear = opts.initYear? parseInt(opts.initYear) : new Date().getFullYear();
-			// 起始月，默认为1月
-			var startMonth = opts.monthRange? parseInt(opts.monthRange[0]) : 1;
-			// 起始日，默认为1月1日
-			var startDate = new Date(initYear, startMonth-1, 1); // 2017-?-1
-			// 结束月，默认为12月
-			var endMonth = opts.monthRange? parseInt(opts.monthRange[1]) : 12;
-			// 这年的1月1日星期几？如果是星期天（0），则返回7
-			var startDay = startDate.getDay() == 0? 7 : startDate.getDay();
-			// 设置一周从哪天开始，星期天为0，星期一为1，以此类推，默认星期一 
-			// var weekStart = parseInt(opts.weekStart) || 1;
-			var weekStart = opts.weekStart !=null && (typeof opts.weekStart=='number'||typeof opts.weekStart=='string')? parseInt(opts.weekStart) : 1;
-			// 设置是否显示周次，默认显示
-			var showWeekNum = opts.showWeekNum || true;
-			// 设置周次从第几周开始，默认从第一周开始
-			var weekNumStart = opts.weekNumStart || 1;
-
-			var htmlStr = '<table>';
+		function joinTableStr() {
+			var htmlStrArr = [];
+			htmlStrArr.push('<table>');
 			// join thead
 			if (showWeekNum === true) {
-				htmlStr += '<thead><tr><th></th><th></th><th colspan="5">'+ initYear +'年</th><th></th><tr><th class="weeknum">周次</th>';
+				htmlStrArr.push('<thead><tr><th></th><th></th><th colspan="5">'+ initYear +'年</th><th></th><tr><th class="weeknum">周次</th>');
 			} else {
-				htmlStr += '<thead><tr><th></th><th colspan="5">'+ initYear +'年</th><th></th><tr>';
+				htmlStrArr.push('<thead><tr><th></th><th colspan="5">'+ initYear +'年</th><th></th><tr>');
 			}
 			var weekArr = ['日', '一', '二', '三', '四', '五', '六'];
 			for (var i = 0, iDay = weekStart; i < 7; i++) {
-				htmlStr += '<th>'+weekArr[iDay]+'</th>';
+				htmlStrArr.push('<th>'+weekArr[iDay]+'</th>');
 				(iDay+1) >= 7 ? iDay=0 : iDay+=1;
 			}
-			htmlStr += '</tr></thead>';
+			htmlStrArr.push('</tr></thead>');
 
 			// join tbody
-			htmlStr += '<tbody>';
+			htmlStrArr.push('<tbody>');
 			// 第几周，从1开始
 			var weekNum = 1;
 			// 第一天前面空多少格
@@ -78,12 +54,12 @@
 					// 周次
 					var wn = (weekNum-weekNumStart+1) < 1? "":(weekNum-weekNumStart+1);
 					// 每行的note
-					// htmlStr += '<td class="note" data-weeknum="' + wn + '"></td>';
+					// htmlStrArr.push('<td class="note" data-weeknum="' + wn + '"></td>');
 					// 是否显示周次
 					if (showWeekNum === true) {
-						htmlStr += '<tr><td class="weeknum">'+ wn +'</td>';
+						htmlStrArr.push('<tr><td class="weeknum">'+ wn +'</td>');
 					} else {
-						htmlStr += '<tr>';
+						htmlStrArr.push('<tr>');
 					}
 					weekNum += 1;
 				}
@@ -119,14 +95,14 @@
 				}
 				// 公历字符串：2017-01-01
 				var caleStr = iDate.getFullYear() + '-' + (iDate.getMonth()+1) + '-' + iDate.getDate();
-				htmlStr += '<td class="' + className + '" data-cale="' + caleStr + '">';
+				htmlStrArr.push('<td class="' + className + '" data-cale="' + caleStr + '">');
 				
 				// 如果这天是1日，就加上月份
 				if (iDate.getDate() == 1) 
-					htmlStr += '<strong class="month">' + (iDate.getMonth()+1) + '</strong>/';
-				htmlStr += iDate.getDate();
+					htmlStrArr.push('<strong class="month">' + (iDate.getMonth()+1) + '</strong>/');
+				htmlStrArr.push(iDate.getDate());
 				
-				// htmlStr += lunar.lunarMonth + "月"; // 农历月份
+				// htmlStrArr.push(lunar.lunarMonth + "月"); // 农历月份
 				// 如果是农历节日，就显示农历节日
 				var lnInfo = '';
 				if (lunar.lunarFestival) {
@@ -140,60 +116,90 @@
 					}
 				}
 				// var lnInfo = lunar.lunarFestival? lunar.lunarFestival : lunar.lunarDay;
-				htmlStr += '<div class="lunarInfo">' + lnInfo + '</div>';
-				htmlStr += '</td>'
+				htmlStrArr.push('<div class="lunarInfo">' + lnInfo + '</div>');
+				htmlStrArr.push('</td>');
 				iDate = new Date(iDate.getFullYear(), iDate.getMonth(), iDate.getDate()+1);
 			}
-			// htmlStr += '<td class="note"></td></tr></tbody>';
-			htmlStr += '</tr></tbody>';
-			var modelStr = 	'<div id="caleSchoModel" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">' +
-								'<div class="modal-dialog modal-sm" role="document">' +
-									'<div class="modal-content">' +
-										'<div class="modal-header">' +
-											'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>' +
-											'<h4 class="modal-title" id="">编辑事件</h4>' +
-										'</div>' +
-										'<div class="modal-body">' +
-											'<form class="form-horizontal" role="form">' +
-												'<div class="form-group">' +
-													'<label class="col-md-3 control-label">时间</label>' +
-													'<div class="col-md-8"><input type="text" class="form-control" readOnly id="caleScho_rangeTime" value=""/></div>' +
-												'</div>' +
-												'<div class="form-group">' +
-													'<label class="col-md-3 control-label">事件名称</label>' +
-													'<div class="col-md-8"><input type="text" class="form-control"  placeholder="事件名称"  id=""/></div>' +
-												'</div>' +
-												'<div class="form-group">' +
-													'<label class="col-md-3 control-label">事件类型</label>' +
-													'<div class="col-md-8">' +
-														'<select class="form-control" id = "" name="">' +
-															'<option value="">请选择</option>' +
-														'</select>' +
-													'</div>' +
-												'</div>' +
-												'<div class="form-group">' +
-													'<label class="col-md-3 control-label">备注</label>' +
-													'<div class="col-md-8"><textarea class="form-control" name="" id="" cols="30" rows="4"></textarea></div>' +
-												'</div>' +
-												'<div class="form-group">' +
-													'<div class="col-md-8 col-md-offset-3">' +
-														'<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>&emsp;<button type="button" class="btn btn-primary">提交</button>' +
-													'</div>' +
-												'</div>' +
-											'</form>' +
-										'</div>' +
-											/*'<div class="modal-footer">' +
-												'<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button><button type="button" class="btn btn-primary">提交</button>' +
-											'</div>' +*/
+			// htmlStrArr.push '<td class="note"></td></tr></tbody>';
+			htmlStrArr.push('</tr></tbody>');
+			var modelStrArr = 	['<div id="caleSchoModel" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">',
+								'<div class="modal-dialog modal-sm" role="document">',
+									'<div class="modal-content">',
+										'<div class="modal-header">',
+											'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>',
+											'<h4 class="modal-title" id="">编辑事件</h4>',
+										'</div>',
+										'<div class="modal-body">',
+											'<form class="form-horizontal" role="form">',
+												'<div class="form-group">',
+													'<label class="col-md-3 control-label">时间</label>',
+													'<div class="col-md-8"><input type="text" class="form-control" readOnly id="caleScho_rangeTime" value=""/></div>',
+												'</div>',
+												'<div class="form-group">',
+													'<label class="col-md-3 control-label">事件名称</label>',
+													'<div class="col-md-8"><input type="text" class="form-control"  placeholder="事件名称"  id=""/></div>',
+												'</div>',
+												'<div class="form-group">',
+													'<label class="col-md-3 control-label">事件类型</label>',
+													'<div class="col-md-8">',
+														'<select class="form-control" id = "" name="">',
+															'<option value="">请选择</option>',
+														'</select>',
+													'</div>',
+												'</div>',
+												'<div class="form-group">',
+													'<label class="col-md-3 control-label">备注</label>',
+													'<div class="col-md-8"><textarea class="form-control" name="" id="" cols="30" rows="4"></textarea></div>',
+												'</div>',
+												'<div class="form-group">',
+													'<div class="col-md-8 col-md-offset-3">',
+														'<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>&emsp;<button type="button" class="btn btn-primary">提交</button>',
+													'</div>',
+												'</div>',
+											'</form>',
+										'</div>',
+											/*'<div class="modal-footer">',
+												'<button type="button" class="btn btn-default" data-dismiss="modal">关闭</button><button type="button" class="btn btn-primary">提交</button>',
+											'</div>',*/
 										
-									'</div>' + 
-								'</div>' + 
-							'</div>';
-			htmlStr += '<tfoot></tfoot>';
-			htmlStr += '</table>' + modelStr;
+									'</div>', 
+								'</div>', 
+							'</div>'];
+			htmlStrArr.push('<tfoot></tfoot>');
+			htmlStrArr.push('</table>');
+			htmlStrArr.push(modelStrArr.join(''))
+			return htmlStrArr.join('');
+		}
+		console.time();
+		
+		var $self = $(this);
+		// arguments[0] is 'options'
+		if (!arguments.length || typeof arguments[0] == 'object') {
+
+			var opts = arguments[0] || {};
+			
+			// 初始年，默认为当前年
+			var initYear = opts.initYear? parseInt(opts.initYear) : new Date().getFullYear();
+			// 起始月，默认为1月
+			var startMonth = opts.monthRange? parseInt(opts.monthRange[0]) : 1;
+			// 起始日，默认为1月1日
+			var startDate = new Date(initYear, startMonth-1, 1); // 2017-?-1
+			// 结束月，默认为12月
+			var endMonth = opts.monthRange? parseInt(opts.monthRange[1]) : 12;
+			// 这年的1月1日星期几？如果是星期天（0），则返回7
+			var startDay = startDate.getDay() == 0? 7 : startDate.getDay();
+			// 设置一周从哪天开始，星期天为0，星期一为1，以此类推，默认星期一 
+			// var weekStart = parseInt(opts.weekStart) || 1;
+			var weekStart = opts.weekStart !=null && (typeof opts.weekStart=='number'||typeof opts.weekStart=='string')? parseInt(opts.weekStart) : 1;
+			// 设置是否显示周次，默认显示
+			var showWeekNum = opts.showWeekNum || true;
+			// 设置周次从第几周开始，默认从第一周开始
+			var weekNumStart = opts.weekNumStart || 1;
+
+			var oStr = joinTableStr();
 			// 渲染dom前的回调函数，参数为该dom的jQuery对象
-			opts.bDrawCallback && typeof opts.bDrawCallback=='function'? opts.bDrawCallback.call($self, $(htmlStr)) :null;
-			$self.html(htmlStr);
+			opts.bDrawCallback && typeof opts.bDrawCallback=='function'? opts.bDrawCallback.call($self, $(oStr)) :null;
+			$self.html(oStr);
 
 			// bind event
 			$('body').on('click', function (e) {
@@ -214,7 +220,8 @@
 					// $('#caleScho_rangeTime').text(valStr);
 					$('#caleSchoModel').modal('show');
 				} else {
-					var editBtn = '<a href="javascript:void(0);" class="caleScho_edit btn btn-success btn-xs"><i class="glyphicon glyphicon-edit"></i></a>';
+					// var editBtn = '<a href="" class="caleScho_edit btn btn-success btn-xs"><i class="glyphicon glyphicon-edit"></i></a>';
+					var editBtn = '<a href="javascript:void(0);" class="caleScho_edit"><i></i></a>';
 					$self.find('.caleScho_edit').remove();
 					$(this).toggleClass('active');
 					$self.find('.active')? $self.find('.active:last').prepend(editBtn) : null;
